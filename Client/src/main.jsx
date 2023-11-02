beb71c815cffd0455b1303de10037c0130cad393
Masxole <macxolelouzaar@gmail.com>
2023-11-02 13:14:29 +0200

    Pages and components added

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Landing from '../Pages/Landing';
+import Login from '../Pages/Login';
+import Dashboard from '../Pages/Dashboard';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/dash",
+    element: <Dashboard />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </React.StrictMode>
+);
