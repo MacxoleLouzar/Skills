2954a1a38f53a14b70fff2ed53ab407c93062400
Masxole <macxolelouzaar@gmail.com>
2023-11-16 21:40:31 +0200

    add css  outlet and fix bugs

--- Client/Pages/Dashboard.jsx
+++ Client/Pages/Dashboard.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import DashCp from "../Components/DashCp";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
 import Sidebar from "../Components/Sidebar";
+import "../src/App.css";
 import DepartmentList from "./DepartmentList";
 import EmployeeList from "./EmployeeList";
 import PositionsList from "./PositionsList";
+
 const Dashboard = () => {
   return (
-    <div>
-      {/* <Header /> */}
+    <>
       <Sidebar />
       <div className="container">
+        <div className="dash">
+          <div className="dashNav">
+            <Link to="/employees">Employeess</Link>
+            <Link to="/departments">Departments</Link>
+            <Link to="/positions">Position</Link>
+          </div>
+          <div className="container">
+            <Outlet />
+          </div>
+        </div>
+
         <Routes>
-          <Route path="dashcp" element={<DashCp />}>
+          <Route path="dash" element={<Dashboard />}>
             <Route path="employees" element={<EmployeeList />} />
             <Route path="departments" element={<DepartmentList />} />
             <Route path="positions" element={<PositionsList />} />
           </Route>
         </Routes>
       </div>
-    </div>
+    </>
   );
 };
 
